5d5e0ff11004bd49f3c122c489214f8f5ef88946
Adedipupo <ajokanola@terragonltd.com>
2023-01-27 09:16:50 +0100

    order-module

---
+++ models/user/orderModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description'],
+      trim: true,
+    }
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const ProductModel = mongoose.model('Products', productSchema)
+
+
+module.exports = ProductModel;
